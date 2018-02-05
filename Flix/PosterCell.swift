dbed73058953bfd80d65af36c4016da7db0f6d2c
Coleman Johnston <john7117@m506-118-26434.local>
2018-02-05 15:50:32 -0800

    Started setting up collection view

---
+++ Flix/PosterCell.swift
@@ -0,0 +1,14 @@
+//
+//  PosterCell.swift
+//  Flix
+//
+//  Created by Coleman Johnston on 2/5/18.
+//  Copyright © 2018 Coleman Johnston. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    @IBOutlet weak var posterImage: UIImageView!
+    
+}
