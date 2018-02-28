12d7a3fdab0f753cbd21bebc25cbff0691b6fb49
Kristine Laranjo <kristine@Kristines-MacBook-Pro.local>
2018-02-28 14:43:23 -0800

    Added movie model

--- Flix/Movie.swift
+++ Flix/Movie.swift
@@ -14,7 +14,15 @@ class Movie {
     
     init(dictionary: [String: Any]) {
         title = dictionary["title"] as? String ?? "No title"
-        
+        let posterPath = dictionary["poster_path"] as? String
+        if let posterPath = posterPath {
+            let baseUrlString = "https://image.tmdb.org/t/p/w500"
+            let fullPosterPath = baseUrlString + posterPath
+            let posterUrl = URL(string: fullPosterPath)
+            if posterUrl != nil {
+                self.posterUrl = posterUrl
+            }
+        }
         // Set the rest of the properties
     }
 }
